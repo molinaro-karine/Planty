0f0cdb93d1906e3efeb14bd4760d3e45ed2f230d
molinaro karine <molinaro.karine@hotmail.com>
2023-06-19 07:02:23 +0200

    Modification du formulaire

--- wp-content/themes/twentytwentyone-child/functions.php
+++ wp-content/themes/twentytwentyone-child/functions.php
@@ -30,29 +30,5 @@ function my_wp_nav_menu_args( $args = '' ) {
 	}
 	add_filter( 'wp_nav_menu_args', 'my_wp_nav_menu_args' );
 
- /*if(is_user_logged_in()) : ?>
-		<div class="user-logged">
-			
-				<a target="_blank" href="<?php echo admin_url(); ?>">Admin</a>
-			
-		</div>
-		<?php endif; ?>*/
 
-		/*
-		<?php
-		add_filter( 'wp_nav_menu_items','add_admin_link', 10, 2 );
-
-		function add_admin_link( $items, $args ) {
-
-    		if (is_user_logged_in() && $args->theme_location == 'menu-1') {
-
-        $items .= '<li><a href="'. get_admin_url() .'">Admin</a></li>';
-
-    }
-
-    return $items;
-
-}
-
-?>*/
 	
